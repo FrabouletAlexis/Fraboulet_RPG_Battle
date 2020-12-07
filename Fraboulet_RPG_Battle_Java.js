e3de8fe72b76c5c85b57da4f9a50f26eb22db55d
FrabouletAlexis <74670007+FrabouletAlexis@users.noreply.github.com>
2020-12-07 01:24:20 +0100

    Create Fraboulet_RPG_Battle_Java.js

---
+++ Fraboulet_RPG_Battle_Java.js
@@ -0,0 +1,55 @@
+var pv_heros_1 = document.getElementById("pv_heros_1");
+var pv_heros_2 = document.getElementById("pv_heros_2");
+var pv_heros_3 = document.getElementById("pv_heros_3");
+var pv_heros_4 = document.getElementById("pv_heros_4");
+
+var pm_heros_1 = document.getElementById("pm_heros_1");
+var pm_heros_2 = document.getElementById("pm_heros_2");
+var pm_heros_3 = document.getElementById("pm_heros_3");
+var pm_heros_4 = document.getElementById("pm_heros_4");
+
+var attaque_heros_1 = document.getElementById("attaque_heros_1");
+var attaque_heros_2 = document.getElementById("attaque_heros_2");
+var attaque_heros_3 = document.getElementById("attaque_heros_3");
+var attaque_heros_4 = document.getElementById("attaque_heros_4");
+
+var defence_heros_1 = document.getElementById("defence_heros_1");
+var defence_heros_2 = document.getElementById("defence_heros_2");
+var defence_heros_3 = document.getElementById("defence_heros_3");
+var defence_heros_4 = document.getElementById("defence_heros_4");
+
+var action_atk_heros_1 = document.getElementById("atk_heros_1");
+var action_atk_heros_2 = document.getElementById("atk_heros_2");
+var action_atk_heros_3 = document.getElementById("atk_heros_3");
+var action_atk_heros_4 = document.getElementById("atk_heros_4");
+
+var action_def_heros_1 = document.getElementById("def_heros_1");
+var action_def_heros_2 = document.getElementById("def_heros_2");
+var action_def_heros_3 = document.getElementById("def_heros_3");
+var action_def_heros_4 = document.getElementById("def_heros_4");
+
+var action_special_heros_1 = document.getElementById("special_heros_1");
+var action_special_heros_2 = document.getElementById("special_heros_2");
+var action_special_heros_3 = document.getElementById("special_heros_3");
+var action_special_heros_4 = document.getElementById("special_heros_4");
+
+
+var nom_monstre_1 = document.getElementsByClassName("nom_monstre_1");
+var nom_monstre_2 = document.getElementsByClassName("nom_monstre_2");
+var nom_monstre_3 = document.getElementsByClassName("nom_monstre_3");
+
+var pv_monstre_1 = document.getElementsByClassName("pv_monstre_1");
+var pv_monstre_2 = document.getElementsByClassName("pv_monstre_2");
+var pv_monstre_3= document.getElementsByClassName("pv_monstre_3");
+
+var attaque_monstre_1 = 10;
+var attaque_monstre_2 = 10;
+var attaque_monstre_3 = 10;
+
+pv_monstre_1 = 130;
+pv_monstre_2 = 130;
+pv_monstre_3 = 130;
+
+nom_monstre_1 = "Shield sheepman";
+nom_monstre_2 = "Blue sheepman";
+nom_monstre_3 = "Elder sheepman";
