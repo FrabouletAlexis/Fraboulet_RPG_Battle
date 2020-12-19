0981ac658673887e6db966c64b7ee82ee90abd23
FrabouletAlexis <74670007+FrabouletAlexis@users.noreply.github.com>
2020-12-19 13:07:03 +0100

    Update Fraboulet_RPG_Battle_Java.js

--- Fraboulet_RPG_Battle_Java.js
+++ Fraboulet_RPG_Battle_Java.js
@@ -1,57 +1,59 @@
+// -------- Stats perso -------- //
 var pv_heros_1 = document.getElementById("pv_heros_1");
-var pv_heros_2 = document.getElementById("pv_heros_2");
-var pv_heros_3 = document.getElementById("pv_heros_3");
-var pv_heros_4 = document.getElementById("pv_heros_4");
-
 var pm_heros_1 = document.getElementById("pm_heros_1");
-var pm_heros_2 = document.getElementById("pm_heros_2");
-var pm_heros_3 = document.getElementById("pm_heros_3");
-var pm_heros_4 = document.getElementById("pm_heros_4");
+var def_heros_1= document.getElementById("defence_heros_1");
+var atk_heros_1 = 10;
 
-var attaque_heros_1 = document.getElementById("attaque_heros_1");
-var attaque_heros_2 = document.getElementById("attaque_heros_2");
-var attaque_heros_3 = document.getElementById("attaque_heros_3");
-var attaque_heros_4 = document.getElementById("attaque_heros_4");
+// -------- Stats monstre -------- //
+var pv_monstre_1 = document.getElementById("pv_monstre_1");
 
-var defence_heros_1 = document.getElementById("defence_heros_1");
-var defence_heros_2 = document.getElementById("defence_heros_2");
-var defence_heros_3 = document.getElementById("defence_heros_3");
-var defence_heros_4 = document.getElementById("defence_heros_4");
+// -------- Boutons -------- //
+var bouton_atk = document.getElementById("atk_heros_1");
+var bouton_def = document.getElementById("def_heros_1");
+var bouton_spe = document.getElementById("special_heros_1");
 
-var action_atk_heros_1 = document.getElementById("atk_heros_1");
-var action_atk_heros_2 = document.getElementById("atk_heros_2");
-var action_atk_heros_3 = document.getElementById("atk_heros_3");
-var action_atk_heros_4 = document.getElementById("atk_heros_4");
+// -------- Boite dialogue -------- //
+var textAction = document.getElementById("boite_dialogue");
 
-var action_def_heros_1 = document.getElementById("def_heros_1");
-var action_def_heros_2 = document.getElementById("def_heros_2");
-var action_def_heros_3 = document.getElementById("def_heros_3");
-var action_def_heros_4 = document.getElementById("def_heros_4");
+// Caractéristiques Personnage // 
+pv_heros_1.value = 100;
+pv_heros_1.innerHTML = pv_heros_1.value;
 
-var action_special_heros_1 = document.getElementById("special_heros_1");
-var action_special_heros_2 = document.getElementById("special_heros_2");
-var action_special_heros_3 = document.getElementById("special_heros_3");
-var action_special_heros_4 = document.getElementById("special_heros_4");
+// Caractéristiques monstre //
+pv_monstre_1.value = 4800;
+pv_monstre_1.innerHTML = pv_monstre_1.value;
 
 
-var nom_monstre_1 = document.getElementsByClassName("nom_monstre_1");
-var nom_monstre_2 = document.getElementsByClassName("nom_monstre_2");
-var nom_monstre_3 = document.getElementsByClassName("nom_monstre_3");
+function atk(){
+		
+	if (bouton_atk.disabled == false){
+		
+		pv_monstre_1.value -= atk_heros_1;
+		bouton_atk.style.opacity = "0.5";
+		bouton_atk.value = "Indisponible";
+		bouton_atk.disabled = true;
+		textAction.innerHTML = "Vous avez infligé "+atk_heros_1+" points de dégats";
+		
+		bouton_def.disabled = true;
+		bouton_spe.disabled = true;
+		
+		bouton_def.style.opacity = "0.5";
+		bouton_spe.style.opacity = "0.5";		
+	}	
+}
 
-var pv_monstre_1 = document.getElementsByClassName("pv_monstre_1");
-var pv_monstre_2 = document.getElementsByClassName("pv_monstre_2");
-var pv_monstre_3= document.getElementsByClassName("pv_monstre_3");
 
-var attaque_monstre_1 = 10;
-var attaque_monstre_2 = 10;
-var attaque_monstre_3 = 10;
+function stat_monstre () {
 
-pv_monstre_1 = 130;
-pv_monstre_2 = 130;
-pv_monstre_3 = 130;
+	var stat=document.getElementById("stat_monstre");
+	if(stat.style.visibility == "hidden") {
+		stat.style.visibility = "visible";
+	} else {
+		stat.style.visibility = "hedden";
+	}
+}
 
-nom_monstre_1 = "Shield sheepman";
-nom_monstre_2 = "Blue sheepman";
-nom_monstre_3 = "Elder sheepman";
 
-pv_heros_1 = 100;
+
+
+bouton_atk.onclick = atk;
