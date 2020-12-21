a7f3747f6bbeaf98d5fe7392c055a717c87e4e06
FrabouletAlexis <74670007+FrabouletAlexis@users.noreply.github.com>
2020-12-21 20:08:36 +0100

    Update Fraboulet_RPG_Battle_Java.js

--- Fraboulet_RPG_Battle_Java.js
+++ Fraboulet_RPG_Battle_Java.js
@@ -1,19 +1,21 @@
 // ----------------------------------------- Boutons ----------------------------------------- //
 var bouton_atk_heros_1 = document.getElementById("atk_heros_1");
 var bouton_def_heros_1 = document.getElementById("def_heros_1");
-var bouton_soin = document.getElementById("special_heros_1");
+var bouton_sepcial_heros_1 = document.getElementById("sepcial_heros_1");
 
 var bouton_atk_heros_2 = document.getElementById("atk_heros_2");
 var bouton_def_heros_2 = document.getElementById("def_heros_2");
-var bouton_balayage = document.getElementById("special_heros_2");
+var bouton_sepcial_heros_2 = document.getElementById("special_heros_2");
 
 var bouton_atk_heros_3 = document.getElementById("atk_heros_3");
 var bouton_def_heros_3 = document.getElementById("def_heros_3");
-var bouton_poison = document.getElementById("special_heros_3");
+var bouton_sepcial_heros_3 = document.getElementById("special_heros_3");
 
 var bouton_atk_heros_4 = document.getElementById("atk_heros_4");
 var bouton_def_heros_4 = document.getElementById("def_heros_4");
-var bouton_tir_charge = document.getElementById("special_heros_4");
+var bouton_sepcial_heros_4 = document.getElementById("special_heros_4");
+
+var bouton_fin_de_tour = document.getElementById("fin_de_tour");
 
 // ----------------------------------------- Boite dialogue ----------------------------------------- //
 
@@ -107,16 +109,19 @@ def_heros_4.innerHTML = def_heros_4.value;
 
 // ----------------------------------------- Caractéristiques monstre ----------------------------------------- //
 // Monstre 1 //
-var atk_monstre_1 = 50;
+var atk_monstre_1 = 80;
 var pv_monstre_1 = document.getElementById("pv_monstre_1");
-pv_monstre_1.value = 4800;
+pv_monstre_1.value = 100;
 pv_monstre_1.innerHTML = pv_monstre_1.value;
-
+// Monstre 2 //
+var atk_monstre_2 = 40;
+var pv_monstre_2 = document.getElementById("pv_monstre_2");
+pv_monstre_2.value = 130;
+pv_monstre_2.innerHTML = pv_monstre_2.value;
 // Monstre 3 //
+var atk_monstre_3 = 20
 var pv_monstre_3 = document.getElementById("pv_monstre_3");
-var monstre_3= document.getElementById("monstre_3");
-var nom_monstre_3 = document.getElementById("nom_monstre_3");
-pv_monstre_3.value = 360;
+pv_monstre_3.value = 100;
 pv_monstre_3.innerHTML = pv_monstre_3.value;
 
 
@@ -124,16 +129,145 @@ function nb_alea(min, max){
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function teste_victoire(){
+	if (pv_monstre_1.value <= 0 && pv_monstre_2.value <= 0 && pv_monstre_3.value <= 0){
+		textAction.innerHTML = "!!! Félicitation vous avez gagnié !!!";
+	}
+}
+
+
+
 function attaque_heros_1(){
 		
 	if (bouton_atk_heros_1.disabled == false){
+		var choix_joueur = prompt("Quel monstre souhaitez-vous attaquer ? 1 - Sheld Sheepman 2 - Blue Sheepman 3 - Armored Sheepman");
+		if (choix_joueur == 1){
+			pv_monstre_1.value -= atk_heros_1.value;
+			pv_monstre_1.innerHTML = pv_monstre_1.value;
+
+			bouton_atk_heros_1.style.opacity = "0.5";
+			bouton_atk_heros_1.disabled = true;
+			bouton_def_heros_1.style.opacity = "0.5";
+			bouton_def_heros_1.disabled = true;
+			bouton_sepcial_heros_1.style.opacity = "0.5";
+			bouton_sepcial_heros_1.disabled = true;
+			textAction.innerHTML = "Kearia inflige "+atk_heros_1.value+" points de dégats au Sheld Sheepman.";
+			if (pv_monstre_1.value < 0){
+				function mort_monstre_1(){
+					pv_monstre_1.innerHTML = 0;
+					textAction.innerHTML = "Kearia a vaincu le Sheld Sheepman."
+				}
+				setTimeout(mort_monstre_1, 1000);
+			}
+		}
+
+		if (choix_joueur == 2){
+			pv_monstre_2.value -= atk_heros_1.value;
+			pv_monstre_2.innerHTML = pv_monstre_2.value;
+
+			bouton_atk_heros_1.style.opacity = "0.5";
+			bouton_atk_heros_1.disabled = true;
+			bouton_def_heros_1.style.opacity = "0.5";
+			bouton_def_heros_1.disabled = true;
+			bouton_sepcial_heros_1.style.opacity = "0.5";
+			bouton_sepcial_heros_1.disabled = true;
+			textAction.innerHTML = "Kearia inflige "+atk_heros_1.value+" points de dégats au Blue Sheepman.";
+			if (pv_monstre_2.value < 0){
+				function mort_monstre_2(){
+					pv_monstre_2.innerHTML = 0;
+					textAction.innerHTML = "Kearia a vaincu le Blue Sheepman."
+				}
+				setTimeout(mort_monstre_2, 1000);
+			}
+		}
+
+		if (choix_joueur == 3){
+			pv_monstre_3.value -= atk_heros_1.value;
+			pv_monstre_3.innerHTML = pv_monstre_3.value;
+
+			bouton_atk_heros_1.style.opacity = "0.5";
+			bouton_atk_heros_1.disabled = true;
+			bouton_def_heros_1.style.opacity = "0.5";
+			bouton_def_heros_1.disabled = true;
+			bouton_sepcial_heros_1.style.opacity = "0.5";
+			bouton_sepcial_heros_1.disabled = true;
+			textAction.innerHTML = "Kearia inflige "+atk_heros_1.value+" points de dégats au Armored Sheepman.";
+			if (pv_monstre_3.value < 0){
+				function mort_monstre_3(){
+					pv_monstre_3.innerHTML = 0;
+					textAction.innerHTML = "Kearia a vaincu l'Armored Sheepman."
+				}
+				setTimeout(mort_monstre_3, 1000);
+			}
+		}
+		setTimeout(teste_victoire, 1000);
+	}	
+}
+
+function attaque_heros_2(){
 		
-		pv_monstre_1.value -= atk_heros_1.value;
-		pv_monstre_1.innerHTML = pv_monstre_1.value;
-		bouton_atk_heros_1.style.opacity = "0.5";
-		
-		bouton_atk_heros_1.disabled = true;
-		textAction.innerHTML = "Vous avez infligé "+atk_heros_1.value+" points de dégats.";
+	if (bouton_atk_heros_2.disabled == false){
+		var choix_joueur = prompt("Quel monstre souhaitez-vous attaquer ? 1 - Sheld Sheepman 2 - Blue Sheepman 3 - Armored Sheepman");
+		if (choix_joueur == 1){
+			pv_monstre_1.value -= atk_heros_2.value;
+			pv_monstre_1.innerHTML = pv_monstre_1.value;
+
+			bouton_atk_heros_2.style.opacity = "0.5";
+			bouton_atk_heros_2.disabled = true;
+			bouton_def_heros_2.style.opacity = "0.5";
+			bouton_def_heros_2.disabled = true;
+			bouton_sepcial_heros_2.style.opacity = "0.5";
+			bouton_sepcial_heros_2.disabled = true;
+			textAction.innerHTML = "Barris inflige "+atk_heros_2.value+" points de dégats au Sheld Sheepman.";
+			if (pv_monstre_1.value < 0){
+				function mort_monstre_1(){
+					pv_monstre_1.innerHTML = 0;
+					textAction.innerHTML = "Barris a vaincu le Sheld Sheepman."
+				}
+				setTimeout(mort_monstre_1, 1000);
+			}
+		}
+
+		if (choix_joueur == 2){
+			pv_monstre_2.value -= atk_heros_2.value;
+			pv_monstre_2.innerHTML = pv_monstre_2.value;
+
+			bouton_atk_heros_2.style.opacity = "0.5";
+			bouton_atk_heros_2.disabled = true;
+			bouton_def_heros_2.style.opacity = "0.5";
+			bouton_def_heros_2.disabled = true;
+			bouton_sepcial_heros_2.style.opacity = "0.5";
+			bouton_sepcial_heros_2.disabled = true;
+			textAction.innerHTML = "Barris inflige "+atk_heros_2.value+" points de dégats au Blue Sheepman.";
+			if (pv_monstre_2.value < 0){
+				function mort_monstre_1(){
+					pv_monstre_1.innerHTML = 0;
+					textAction.innerHTML = "Barris a vaincu le Blue Sheepman."
+				}
+				setTimeout(mort_monstre_1, 1000);
+			}
+		}
+
+		if (choix_joueur == 3){
+			pv_monstre_3.value -= atk_heros_2.value;
+			pv_monstre_3.innerHTML = pv_monstre_3.value;
+
+			bouton_atk_heros_2.style.opacity = "0.5";
+			bouton_atk_heros_2.disabled = true;
+			bouton_def_heros_2.style.opacity = "0.5";
+			bouton_def_heros_2.disabled = true;
+			bouton_sepcial_heros_2.style.opacity = "0.5";
+			bouton_sepcial_heros_2.disabled = true;
+			textAction.innerHTML = "Barris inflige "+atk_heros_1.value+" points de dégats au Armored Sheepman.";
+			if (pv_monstre_3.value < 0){
+				function mort_monstre_3(){
+					pv_monstre_3.innerHTML = 0;
+					textAction.innerHTML = "Barris a vaincu l'Armored Sheepman."
+				}
+				setTimeout(mort_monstre_3, 1000);
+			}
+		}
+		setTimeout(teste_victoire, 1000);
 	}	
 }
 
@@ -143,56 +277,159 @@ function defence_heros_1(){
 		bouton_def_heros_1.style.opacity = "0.5";
 
 		bouton_def_heros_1.disabled = true;
-		textAction.innerHTML = "Vous résistérez à "+def_heros_1.value+" points de dégats si vous été attaqué."
+		textAction.innerHTML = "Kearia augmente sa défense de "+def_heros_1.value+" points pendant ce tour."
 	}
 }
 
+/*function soin (){
+	var heros_en_vie = 0;
+	if (pv_heros_1.value>0){
+		heros_en_vie += 1;
+	}
+	if (pv_heros_2.value>0){
+		heros_en_vie += 1;
+	}
+	if (pv_heros_3.value>0){
+		heros_en_vie += 1;
+	}
+	if (pv_heros_4.value>0){
+		heros_en_vie += 1;
+	}
+
+	for (i=heros_en_vie){
+
+	}
+}*/
 function soin(){
-	if (bouton_soin.disabled == false){
+		
+	if (bouton_sepcial_heros_1.disabled == false){
 		if (pm_heros_1.value > mana_soin){
-			if(pv_heros_1.value < pv_heros_2.value ){
+
+			
+			if(pv_heros_1.value > 0 && pv_heros_1.value < pv_heros_2.value) {
 				if (pv_heros_1.value < pv_max_heros_1){
-					pv_heros_1.value = pv_heros_1.value + pv_soin;
+					pv_heros_1.value = pv_heros_1.value+ pv_soin;
+					pv_heros_1.innerHTML = pv_heros_1.value;
 					pm_heros_1.value = pm_heros_1.value - mana_soin;
-					textAction.innerHTML = "Kearia regagne"+pv_soin+" points de vie.";
+					pm_heros_1.innerHTML = pm_heros_1.value;
+					textAction.innerHTML = "Kearia regagne "+pv_soin+" points de vie.";
+					bouton_sepcial_heros_1.style.opacity = "0.5";
+					bouton_sepcial_heros_1.disabled = true;
 					if (pv_heros_1.value > pv_max_heros_1){
-						pv_heros_1 = pv_max_heros_1;
+						pv_heros_1.value = pv_max_heros_1;
+						pv_heros_1.innerHTML = pv_heros_1.value;
 					}
-					bouton_atk_heros_1.style.opacity = "0.5";
-					bouton_atk_heros_1.disabled = true;
 				}
+	
+			}else if (pv_heros_2.value > 0 && pv_heros_2.value < pv_heros_1.value){
+
+				if (pv_heros_2.value < pv_max_heros_2){
+					pv_heros_2.value = pv_heros_2.value + pv_soin;
+					pv_heros_2.innerHTML = pv_heros_2.value;
+					pm_heros_1.value = pm_heros_1.value - mana_soin;
+					pm_heros_1.innerHTML = pm_heros_1.value;
+					textAction.innerHTML = "Barris regagne "+pv_soin+" points de vie.";
+					bouton_sepcial_heros_1.style.opacity = "0.5";
+					bouton_sepcial_heros_1.disabled = true;
+					if (pv_heros_2.value > pv_max_heros_2){
+						pv_heros_2.value = pv_max_heros_2;
+						pv_heros_2.innerHTML = pv_heros_2.value;
+					}
+				}
+			} else if (pv_heros_2.value == pv_max_heros_2||pv_heros_2.value<=0) {
+				textAction.innerHTML = "Il n'y a personne a soigné.";
 			}
+
+		} else {
+			textAction.innerHTML = "Kearia n'a plus assez de mana.";
 		}
 	}
 }
-
 function balayage (){}
 
 function poison (){}
 
 function tir_charge () {}
  
-function attaque_monstre_1(){
-	if (pv_monstre_1 > 0) {
-		var heros_en_vie = 0;
-		if (pv_heros_1 > 0){
-			heros_en_vie = heros_en_vie +1;
-		}
 
-		var cible_monstre_1 = nb_alea(1,heros_en_vie);
-			if (cible_monstre_1 == 1) {
-				pv_heros_1.value = pv_heros_1 -(atk_monstre_1-def_valeur_heros_1);
-				textAction.innerHTML = "Vous subissez "+atk_monstre_1+" points de dégats.";
-				if (pv_heros_1.value == 0){
-					textAction.innerHTML = "Vous avez sucombez"
+function fin_de_tour(){
+	var heros_en_vie = 0;
+	if (pv_heros_1.value > 0){
+		heros_en_vie += 1;
+	}
+
+	/*if (pv_heros_2.value > 0){
+		heros_en_vie += 1;
+	}*/
+	function attaque_monstre_1(){
+		if (pv_monstre_1.value > 0){
+			var cible_monstre = nb_alea(1,heros_en_vie);
+			if (cible_monstre == 1){
+				pv_heros_1.value = (pv_heros_1.value + def_valeur_heros_1) -  atk_monstre_1;
+				pv_heros_1.innerHTML = pv_heros_1.value;
+				textAction.innerHTML = "Sheld Sheepman inflige "+atk_monstre_1+" points de dégats à Kearia.";
+				if (pv_heros_1.value < 0){
+					function mort_kearia (){
+						textAction.innerHTML = "Kearia a succombé.";
+					}
+					setTimeout(mort_kearia, 1000);
+					pv_heros_1.innerHTML = "DEAD";
+					pm_heros_1.innerHTML = 0;
+					atk_heros_1.innerHTML = 0;
+					def_heros_1.innerHTML = 0;
+					bouton_atk_heros_1.style.opacity = "0.5";
+					bouton_atk_heros_1.disabled = true;
+					bouton_def_heros_1.style.opacity = "0.5";
+					bouton_def_heros_1.disabled = true;
+					bouton_sepcial_heros_1.style.opacity = "0.5";
+					bouton_sepcial_heros_1.disabled = true;
 				}
 			}
-	} 
+			/*if (cible_monstre == 2){
+				pv_heros_2.value -= atk_monstre_1;
+				pv_heros_2.innerHTML = pv_heros_2.value;
+				textAction.innerHTML = "Sheld Sheepman inflige "+atk_monstre_1+" points de dégats a Barris.";
+			}*/
+		}
+	}
+	
+	function reactivation_bouton (){
+		if (pv_heros_1.value > 0){
+			if (bouton_atk_heros_1.disabled == true){
+				bouton_atk_heros_1.style.opacity = "1.5";
+				bouton_atk_heros_1.disabled = false;
+			}
+			if (bouton_def_heros_1.disabled == true){
+				bouton_def_heros_1.style.opacity = "1.5";
+				bouton_def_heros_1.disabled = false;
+			}
+			if (bouton_sepcial_heros_1.disabled == true){
+				bouton_sepcial_heros_1.style.opacity = "1.5";
+				bouton_sepcial_heros_1.disabled = false;
+			}
+		}
+	}
+	
+	setTimeout(attaque_monstre_1, 500);
+
+	var defaite = function(){
+		if (pv_heros_1.value <= 0 && pv_heros_2<= 0 && pv_heros_3.value <=0 && pv_heros_4.value <=0 ){
+			textAction.innerHTML = " Vous avez tous péri ";
+		} else {
+			return false;
+		}
+	}
+	if (defaite == false){
+		setTimeout(reactivation_bouton, 500);
+	}
+
 }
 
 
 
-attaque_monstre_1();
+
 bouton_atk_heros_1.onclick = attaque_heros_1;
+bouton_atk_heros_2.onclick = attaque_heros_2;
 bouton_def_heros_1.onclick = defence_heros_1;
-bouton_soin.onclick = soin;
+bouton_sepcial_heros_1.onclick = soin;
+bouton_fin_de_tour.onclick = fin_de_tour;
